877fd9b1e12cc5044eaa1cf6a497d13cd4577190
Giovanni Torelli <giovanni.walter@outlook.com>
2024-01-17 10:40:50 -0300

    Created Produto and Categoria repositories

---
+++ src/main/java/com/generation/apigamestore/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.apigamestore.repository;
+
+import com.generation.apigamestore.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
