ae075a8e73bbf35aae594c306196c58b1e1fa54f
Giovanni Torelli <giovanni.walter@outlook.com>
2024-01-17 09:12:04 -0300

    Initial commit

---
+++ src/main/java/com/generation/apigamestore/ApiGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.generation.apigamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGameStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/apigamestore/ApiGameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.apigamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
