952ed04259d191a1ae1c4061fc1d4ec4b3aea534
Giovanni Torelli <giovanni.walter@outlook.com>
2024-01-17 10:31:41 -0300

    Added NotBlank annotation to categoria attribute

--- src/main/java/com/generation/apigamestore/model/Produto.java
+++ src/main/java/com/generation/apigamestore/model/Produto.java
@@ -2,6 +2,7 @@ package com.generation.apigamestore.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
@@ -22,5 +23,6 @@ public class Produto {
     @Positive(message = "O preço deve ser maior do que zero!")
     private BigDecimal preco;
 
+    @NotBlank(message = "O campo categoria é obrigatório!")
     private Long categoria;
 }
