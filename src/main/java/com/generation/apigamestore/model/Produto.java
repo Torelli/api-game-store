b6b4fb64a3914602e11d0211077f00f386b826ac
Giovanni Torelli <giovanni.walter@outlook.com>
2024-01-17 10:48:03 -0300

    Added getters and setters to Produto

--- src/main/java/com/generation/apigamestore/model/Produto.java
+++ src/main/java/com/generation/apigamestore/model/Produto.java
@@ -6,11 +6,15 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "tb_produtos")
+@Getter
+@Setter
 public class Produto {
 
     @Id
